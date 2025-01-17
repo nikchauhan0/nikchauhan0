b9c67021644d716f64b185075ec4b7de0ed9c8be
nikchauhan0 <chauhannikhil278@gmail.com>
2025-01-17 11:17:53 +0530

    first commit

---
+++ demo.py
@@ -0,0 +1,19 @@
+ 
+def add(n1,n2):
+    return n1+n2
+def add(n1,n2):
+    return n1-n2
+def add(n1,n2):
+    return n1*n2
+def add(n1,n2):
+    return n1/n2
+
+class a:
+    pass
+Name="Nikhil"
+Age=24
+
+
+
+
+
